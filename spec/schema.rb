5ff9ad0c36016bdb937a9c580a1137a4e2c97e06
JH. CHABRAN <jh@chabran.fr>
2009-04-14 18:37:39 +0200

    removed useless attributes

--- spec/schema.rb
+++ spec/schema.rb
@@ -1,16 +1,13 @@
 ActiveRecord::Schema.define :version => 0 do
   create_table "users", :force => true do |t|
-    t.integer :user_id
     t.string :login
   end
   
   create_table "foos", :force => true do |t|
-    t.integer :foo_id
     t.string :message
   end
   
   create_table "sights", :force => true do |t|
-    t.integer :sigh_id
     t.string :sightable_type
     t.integer :sightable_id
     t.integer :viewer_id
